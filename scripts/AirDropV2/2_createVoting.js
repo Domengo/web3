64fc28a5201f2799cc32d648d26a04ed143fb928
Domengo <dominicsengo@mail.com>
2024-04-20 16:53:13 +0300

    Add createVoting function to AirDropV2 script

---
+++ scripts/AirDropV2/2_createVoting.js
@@ -0,0 +1,22 @@
+// Importing dependencies
+const { getEncodedData } = require("@q-dev/gdk-sdk");
+const { ethers } = require("hardhat");
+require('dotenv').config();
+const { MODULE_NAME, MAIN_DAO_VOTING_ADDRESS, VOTING_CONTRACT_ADDRESS, TEN_PERCENTAGE, DAO_REGISTRY_NAME } = process.env;
+
+function buildVotingSituation(name, target) {
+    return {
+        votingSituationName: name,
+        votingValues: {
+            votingPeriod: 300,
+            vetoPeriod: 60,
+            proposalExecutionPeriod: 1000,
+            requiredQuorum: TEN_PERCENTAGE,
+            requiredMajority: TEN_PERCENTAGE,
+            requiredVetoQuorum: TEN_PERCENTAGE,
+            votingType: 0,
+            votingTarget: target,
+            votingMinAmount: 1,
+        },
+    };
+}
