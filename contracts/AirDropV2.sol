a8459103739ff6547c26bf70437f1d7366de5a8b
Domengo <dominicsengo@mail.com>
2024-04-20 16:22:50 +0300

    Update AirDropV1 scripts and import statements

---
+++ contracts/AirDropV2.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity ^0.8.19;
+
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {IDAOResource} from "@q-dev/gdk-contracts/interfaces/IDAOResource.sol";
+import {ACampaignAirDrop} from "./libs/ACampaignAirDrop.sol";
--- scripts/AirDropV1/2_deploy_airdropV1.js
+++ scripts/AirDropV1/2_deploy_airdropV1.js
@@ -7,32 +7,53 @@ const { MerkleTree } = require("merkletreejs");
 const fs = require("fs");
 
 async function main() {
-  const qrc20Address = process.env.QRC20_TOKEN;
-  const QRC20 = await ethers.getContractFactory("QRC20");
-  const contract = QRC20.attach(qrc20Address);
+    const qrc20Address = process.env.QRC20_TOKEN;
+    const QRC20 = await ethers.getContractFactory("QRC20");
+    const contract = QRC20.attach(qrc20Address);
 
-  // Merkle Root
-  let addresses = [
-    {
-          addr: "0x60FB4B324624A97E97229F82D7794DC196373aB4",
-    },
-    {
-        addr: "0x409e242b785e437b768cfe53dc8a512677cd11130f6ac0156ca0ca5a0d922c9c",
-    },
-  ];
+    // Merkle Root
+    let addresses = [
+        {
+            addr: "0x60FB4B324624A97E97229F82D7794DC196373aB4",
+        },
+        {
+            addr: "0x409e242b785e437b768cfe53dc8a512677cd11130f6ac0156ca0ca5a0d922c9c",
+        },
+    ];
 
-  const leafNodes = addresses.map((address) =>
-    keccak256(Buffer.from(address.addr.replace("0x", ""), "hex"))
-  );
+    const leafNodes = addresses.map((address) =>
+        keccak256(Buffer.from(address.addr.replace("0x", ""), "hex"))
+    );
 
-  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-  root = merkleTree.getHexRoot();
+    const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+    root = merkleTree.getHexRoot();
 
-  const data = {
-    addresses: addresses.map((address) => address.addr),
-    leafNodes: leafNodes,
-    root: root,
-  };
+    const data = {
+        addresses: addresses.map((address) => address.addr),
+        leafNodes: leafNodes,
+        root: root,
+    };
 
-  fs.writeFileSync("tree.json", JSON.stringify(data, null, 2));
+    fs.writeFileSync("tree.json", JSON.stringify(data, null, 2));
+
+    console.log("Deploying Airdrop V1...");
+
+    const AirDropV1 = await ethers.getContractFactory("AirDropV1");
+    const dropAmt = Web3.utils.toWei('20', 'ether')
+    const airdrop = await AirDropV1.deploy();
+    await airdrop.waitForDeployment();
+    console.log("AirdropV1 deployed to:", await airdrop.getAddress());
+
+    await airdrop.create_airdrop(qrc20Address, dropAmt, root);
+
+    console.log("Funding the Airdrop");
+
+    // mint token
+    const airdropAddress = airdrop.getAddress();
+    const mintAmount = Web3.utils.toWei('2000', 'ether');
+    await contract.mintTo(airdropAddress, mintAmount);
+    console.log("Airdrop Funded...");
+
 }
+
+main()
